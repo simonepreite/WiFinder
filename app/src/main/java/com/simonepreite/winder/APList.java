9996cd70f7b3a0844be7b751903366c6f0c40010
simone preite <simone-preite_89@hotmail.it>
2017-07-07 00:24:52 +0200

    fab button in both activity layout and listener to change activity to map

--- app/src/main/java/com/simonepreite/winder/APList.java
+++ app/src/main/java/com/simonepreite/winder/APList.java
@@ -1,12 +1,15 @@
 package com.simonepreite.winder;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 public class APList extends AppCompatActivity {
 
@@ -26,5 +29,14 @@ public class APList extends AppCompatActivity {
             setContentView(R.layout.activity_aplist);
             Wmanager.startScan();
         }
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabMap1);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Click action
+                Intent intent = new Intent(APList.this, APMaps.class);
+                startActivity(intent);
+            }
+        });
     }
 }
